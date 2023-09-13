6c364234378f7db0e1e86fdbe3880933c3a7262a
Soroush Beigi <sbeygi712@gmail.com>
2023-09-13 14:27:21 +0430

    adding repository and usecases to dependency injections file

--- lib/injections.dart
+++ lib/injections.dart
@@ -1,5 +1,11 @@
 import 'package:get_it/get_it.dart';
 import 'package:weather/features/feature_bookmark/data/data_source/local/database.dart';
+import 'package:weather/features/feature_bookmark/data/repository/city_repository_impl.dart';
+import 'package:weather/features/feature_bookmark/domain/repository/city_repository.dart';
+import 'package:weather/features/feature_bookmark/domain/usecases/all_cities_usecase.dart';
+import 'package:weather/features/feature_bookmark/domain/usecases/delete_city_usecase.dart';
+import 'package:weather/features/feature_bookmark/domain/usecases/save_city_usecase.dart';
+import 'package:weather/features/feature_bookmark/domain/usecases/search_city_usecase.dart';
 import 'package:weather/features/feature_weather/data/data_source/remote/api_provider.dart';
 import 'package:weather/features/feature_weather/data/repository/weather_repository_impl.dart';
 import 'package:weather/features/feature_weather/domain/repository/weather_repository.dart';
@@ -14,12 +20,16 @@ setup() async {
   locator.registerSingleton<ApiProvider>(ApiProvider());
 
   //database
-  final database = await $FloorAppDatabase.databaseBuilder('app_database.db').build();
+  final database =
+      await $FloorAppDatabase.databaseBuilder('app_database.db').build();
   locator.registerSingleton<AppDatabase>(database);
 
   //repositories
   locator.registerSingleton<WeatherRepository>(WeatherRepositoryImplementation(
     apiProvider: locator(),
+  ));
+   locator.registerSingleton<CityRepository>(CityRepositoryImplementation(
+    cityDao: database.cityDao,
   ));
 
   //use cases
@@ -29,6 +39,22 @@ setup() async {
   locator.registerSingleton<GetForecastUseCase>(GetForecastUseCase(
     weatherRepository: locator(),
   ));
+
+
+    locator.registerSingleton<AllCitiesUseCase>(AllCitiesUseCase(
+    cityRepository: locator(),
+  ));
+    locator.registerSingleton<DeleteCityUsecase>(DeleteCityUsecase(
+    cityRepository: locator(),
+  ));
+     locator.registerSingleton<SaveCityUsecase>(SaveCityUsecase(
+    cityRepository: locator(),
+  ));
+     locator.registerSingleton<SearchCityUsecase>(SearchCityUsecase(
+    cityRepository: locator(),
+  ));
+
+  
   //blocs
   locator.registerSingleton<WeatherBloc>(
     WeatherBloc(
